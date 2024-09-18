cc3e8dea91283b17887078539f0e8fadc6965203
Qosam217 <qosamimaduddin7@gmail.com>
2024-09-18 10:19:20 +0700

    day2_3

---
+++ day 2/InRange.java
@@ -0,0 +1,19 @@
+public class InRange {
+    public static void main(String[] args) {
+        int num1 = 32;
+        int num2 = 62;
+        System.out.println(inRange(num1, num2));
+    }
+
+    public static boolean inRange(int n1, int n2){
+        if(n1 >= 30 && n1 <= 50){
+            if(n2 >= 30 && n2 <= 50){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+}
